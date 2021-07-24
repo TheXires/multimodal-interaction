32867c9db8ef8ad5108d0dbfd84933482fe8b38b
Felix Thomsen <14091540+m1212e@users.noreply.github.com>
2021-07-24 20:31:20 +0200

    Cleanup
    Former-commit-id: 2ced81f07d135071011fba7cc88e56caf94d7a99

--- touchcontroller/src/components/controllersection/3d/car.tsx
+++ touchcontroller/src/components/controllersection/3d/car.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLoader } from '@react-three/fiber';
 import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
-import * as modelPath from '../../../../assets/Urus.fbx';
+import * as modelPath from '../../../assets/Urus.fbx';
 
 /**
  * The own car placed in the middle of the scene
--- touchcontroller/src/components/controllersection/Controllersection.tsx
+++ touchcontroller/src/components/controllersection/Controllersection.tsx
@@ -1,22 +1,36 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
 import { Canvas } from '@react-three/fiber';
 
 import './Controllersection.css';
 import Street from './3d/sctreet';
-import Camera from './3d/camera';
-import Vehicles from './3d/vehicles/vehicles';
+import OwnCar from './3d/car';
+import { useThree } from '@react-three/fiber';
+
+function Scene() {
+  const { camera } = useThree();
+  useEffect(() => {
+    camera.position.set(0, 3.5, -4);
+    camera.rotateY(-Math.PI);
+    camera.rotateX(-Math.PI * .2);
+  }, []);
+
+  return (
+    <React.Fragment>
+      <color attach="background" args={['#c7c7c7']} />
+      <ambientLight />
+      <Suspense fallback={null}>
+        <OwnCar />
+      </Suspense>
+      <Street />
+    </React.Fragment>
+  );
+}
 
 function Controllersection() {
   return (
     <div className="controllersection">
       <Canvas dpr={[1, 2]} camera={{ fov: 60 }}>
-        <color attach="background" args={['#c7c7c7']} />
-        <ambientLight />
-        <Camera locked={false} />
-        <Suspense fallback={null}>
-          <Vehicles />
-        </Suspense>
-        <Street />
+        <Scene />
       </Canvas>
     </div>
   );
